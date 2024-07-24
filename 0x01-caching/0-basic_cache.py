56c7fd63bd17b8fe253bd3856aa3dcfbcf6b09f4
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-07-23 23:34:23 -0700

    Basic caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Basic Caching module
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """a caching system
+      must use self.cache_data
+    """
+    def __init__(self):
+        """Initialize"""
+        super().__init__()
+
+    def put(self, key, item):
+        """the dictionary self.cache_data the item value for the key key"""
+        if key in self.cache_data:
+            self.cache_data[key] = item
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """the value in self.cache_data linked to key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
