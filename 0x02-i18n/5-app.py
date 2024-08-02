21b5ffa9ed0b3a53c00b8d443ffedd7d8b971380
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-08-02 02:43:25 -0700

    5-app.py

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,34 +1,29 @@
 #!/usr/bin/env python3
-"""Flask app module"""
-from flask import (
-    Flask,
-    render_template,
-    request,
-    g,
-)
-from flask_babel import Babel, _
+"""A Basic Flask app with internationalization support.
+"""
+from flask_babel import Babel
 from typing import Union, Dict
+from flask import Flask, render_template, request, g
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
-
-
-class Config:
-    """configure available languages"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
-babel = Babel(app)
 
 
 def get_user() -> Union[Dict, None]:
@@ -50,23 +45,20 @@ def before_request() -> None:
 
 @babel.localeselector
 def get_locale() -> str:
-    """Find best match with our supported languages."""
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
+    """Retrieves the locale for a web page.
+    """
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
         return locale
-    if g.user and g.user['locale'] in app.config['LANGUAGES']:
-        return g.user['locale']
-    head_locale = request.args.get('locale', '')
-    if head_locale in app.config['LANGUAGES']:
-        return head_locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route("/")
-def home() -> str:
-    """home page"""
-    return render_template("5-index.html")
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('5-index.html')
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
