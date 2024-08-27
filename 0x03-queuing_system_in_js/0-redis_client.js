60596dc61c8c2c5054fa83bedf0a98639d68febc
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-08-27 11:00:51 -0700

    Redis

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,13 @@
+import { createClient } from 'redis';
+
+const client = await createClient();
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+await client.connect();
